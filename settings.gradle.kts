d6b0ba5496925132ac954e1fd59510aa393e258e
Ilyandr <doc77776@gmail.com>
2022-12-09 00:20:30 +0300

    Create project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "StavLenta"
+include(":androidApp")
+include(":shared")
